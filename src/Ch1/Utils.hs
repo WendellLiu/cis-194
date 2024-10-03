5d0896139714fa4e0fb6ab142168344efc3b6c03
Wendell Liu <wendell.liu@houzz.com>
2024-10-03 15:33:45 +0800

    feat: ex2

--- src/Ch1/Utils.hs
+++ src/Ch1/Utils.hs
@@ -6,12 +6,24 @@ module Ch1.Utils (
 
 toDigits :: Integer -> [Integer]
 toDigits num 
-  | num < 0 = [] 
+  | num < 0 = []
   | num == 0 = []
 toDigits num  = toDigits ( num `div` 10 ) ++ [num `mod` 10]
 
 toDigitsRev :: Integer -> [Integer]
-toDigitsRev = reverse . toDigits 
+toDigitsRev = reverse . toDigits
+
+calcFunc :: Int -> Integer -> Integer 
+calcFunc index ele
+  | even index = ele
+  | otherwise = ele * 2
+
+doubleAndConcatWhenIndexMatch :: (Int -> Integer -> Integer) -> Integer -> (Int, [Integer]) -> (Int, [Integer])
+doubleAndConcatWhenIndexMatch eleFunc number (index, acc) = do
+  let newNumber = eleFunc index number
+  (index + 1, acc ++ [newNumber])
 
 doubleEveryOther :: [Integer] -> [Integer]
-doubleEveryOther arr = arr
+doubleEveryOther arr = do
+  let result = foldr (doubleAndConcatWhenIndexMatch calcFunc) (0, []) arr
+  reverse (snd result)
